2da9614a4bde9348811cc64b64ff94d21dcdb96f
Muhammetcanpolat <73015593+mmc6185@users.noreply.github.com>
2023-10-21 11:10:32 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -4,9 +4,12 @@ import subprocess
 
 mac = input("Mac adresi girin : ")
 
-subprocess.call("ifconfig eth0 down", shell=True)
 
-change_mac = "ifconfig eth0 hw ether " + mac
-subprocess.call(change_mac, shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call(["ifconfig", "eth0", "down"], shell=True)
+subprocess.call(["ifconfig", "eth0", "hw", "ether", mac], shell=True)
+subprocess.call(["ifconfig", "eth0", "up"], shell=True)
+
+
+
+
 
